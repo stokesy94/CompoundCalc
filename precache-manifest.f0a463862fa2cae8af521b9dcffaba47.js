2ed5a481371b6fd049208546733e3bf8e9451a7a
unknown <mattstokes94@yahoo.co.ukgit config --global user.name Stokesy94>
2020-06-05 18:34:24 +0100

    Updates

---
+++ precache-manifest.f0a463862fa2cae8af521b9dcffaba47.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9104f0c4ed2c35b5e2befc71514459e4",
+    "url": "/CompoundCalc/index.html"
+  },
+  {
+    "revision": "1c5012048f05838b7216",
+    "url": "/CompoundCalc/static/css/main.77252cb3.chunk.css"
+  },
+  {
+    "revision": "e8c23b108c8900778491",
+    "url": "/CompoundCalc/static/js/2.bef7c2c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CompoundCalc/static/js/2.bef7c2c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c5012048f05838b7216",
+    "url": "/CompoundCalc/static/js/main.7c875499.chunk.js"
+  },
+  {
+    "revision": "ea72fca2a638184cd0d9",
+    "url": "/CompoundCalc/static/js/runtime-main.ea62ac9c.js"
+  },
+  {
+    "revision": "56e14eb27edab6d30b9c4b2a17653000",
+    "url": "/CompoundCalc/static/media/Colors.56e14eb2.ttf"
+  },
+  {
+    "revision": "41f79afef3da102141fe5b23d63d507a",
+    "url": "/CompoundCalc/static/media/QuartzoBold-W9lv.41f79afe.ttf"
+  },
+  {
+    "revision": "8228b68f18634d3d76a7a295c751a6fd",
+    "url": "/CompoundCalc/static/media/numbers.8228b68f.ttf"
+  }
+]);

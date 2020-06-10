76a3c2db195a1ffa82778d0403b4820f99724180
unknown <mattstokes94@yahoo.co.ukgit config --global user.name Stokesy94>
2020-06-10 15:18:45 +0100

    cleaner code

--- src/Compound.js
+++ src/Compound.js
@@ -6,64 +6,59 @@ class Compound extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            SA: 0,
-            P: 0,
-            IR: 0,
-            C: 0,
-            FV: 0
+            StartingAmount: 0,
+            Period: 0,
+            InterestRate: 0,
+            Contributions: 0,
         }
         this.changeSAHandler = this.changeSAHandler.bind(this);
         this.changePHandler = this.changePHandler.bind(this);
         this.changeIRHandler = this.changeIRHandler.bind(this);
         this.changeCHandler = this.changeCHandler.bind(this);
-        this.changeFVHandler = this.changeFVHandler.bind(this);
+
 
     }
 
     changeSAHandler(e) {
-        this.setState({SA: e.target.value});
+        this.setState({StartingAmount: e.target.value});
     }
 
     changePHandler(e) {
-        this.setState({P: e.target.value});
+        this.setState({Period: e.target.value});
     }
 
     changeIRHandler(e) {
-        this.setState({IR: e.target.value});
+        this.setState({InterestRate: e.target.value});
     }
 
     changeCHandler(e) {
-        this.setState({C: e.target.value});
+        this.setState({Contributions: e.target.value});
     }
     changeFVHandler(e) {
-        this.setState({FV: e.target.value});
+        this.setState({FinalValue: e.target.value});
     }
 
     Yearly() {
-        const one = (this.state.SA / 1 * Math.pow((1 + this.state.IR / 100 / 1), 1 * this.state.P));
-        const two = this.state.C * 12/1 * ((Math.pow((1 + this.state.IR / 100 / 1), 1 * this.state.P) - 1) / (this.state.IR / 100 / 1));
+        const one = (this.state.StartingAmount / 1 * Math.pow((1 + this.state.InterestRate / 100 / 1), 1 * this.state.Period));
+        const two = this.state.Contributions * 12/1 * ((Math.pow((1 + this.state.InterestRate / 100 / 1), 1 * this.state.Period) - 1) / (this.state.InterestRate / 100 / 1));
 
         return one + two;
     }
 
     Monthly() {
-        const four = (this.state.SA / 1 * Math.pow((1 + this.state.IR / 100 / 12), 12 * this.state.P));
-        const five = (this.state.C / 1) * ((Math.pow((1 + this.state.IR / 100 / 12), 12 * this.state.P) - 1) / (this.state.IR / 100 / 12));
+        const four = (this.state.StartingAmount / 1 * Math.pow((1 + this.state.InterestRate / 100 / 12), 12 * this.state.Period));
+        const five = (this.state.Contributions / 1) * ((Math.pow((1 + this.state.InterestRate / 100 / 12), 12 * this.state.Period) - 1) / (this.state.InterestRate / 100 / 12));
 
         return four + five;
     }
 
     totalInterest() {
-        let three = this.Yearly() - (this.state.SA/1 + (this.state.C * 12 * this.state.P))
+        let three = this.Yearly() - (this.state.StartingAmount/1 + (this.state.Contributions * 12 * this.state.Period))
 
         return three;
         }
 
-    // futureValue() {
-    //     const six = this.state.FV/Math.pow((1+(this.state.IR/100/1)/12),12 * this.state.P)
-    //
-    //         return six;
-    // }
+
 
 
     render () {
@@ -74,43 +69,37 @@ class Compound extends Component {
                 <div className="inputs">
                 <h2 className="slideh">Starting Amount:
                     <div className="sliders">
-                        <input type="range" step="10" min="0" max="100000" value={this.state.SA} onChange={this.changeSAHandler} className="slide" />
-                        <input type="number" value={this.state.SA} />
+                        <input type="range" step="10" min="0" max="100000" value={this.state.StartingAmount} onChange={this.changeSAHandler} className="slide" />
+                        <input type="number" value={this.state.StartingAmount} />
                     </div>
                 </h2>
                 <h2 className="slideh"v>Period (years):
                     <div className="sliders">
-                         <input type="range" step="1" min="0" max="60" value={this.state.P} onChange={this.changePHandler} className="slide" />
-                         <input type="number" value={this.state.P} />
+                         <input type="range" step="1" min="0" max="60" value={this.state.Period} onChange={this.changePHandler} className="slide" />
+                         <input type="number" value={this.state.Period} />
                     </div>
                 </h2>
                 <h2 className="slideh">Interest Rate: (%)
                     <div className="sliders">
-                        <input type="range" step=".1" min="0" max="10" value={this.state.IR} onChange={this.changeIRHandler} className="slide" />
-                        <input type="number" value={this.state.IR} />
+                        <input type="range" step=".1" min="0" max="10" value={this.state.InterestRate} onChange={this.changeIRHandler} className="slide" />
+                        <input type="number" value={this.state.InterestRate} />
                     </div>
                 </h2>
                 <h2 className="slideh">Contributions (monthly):
                     <div className="sliders">
-                        <input type="range" step="1" min="0" max="2000" value={this.state.C} onChange={this.changeCHandler} className="slide" />
-                        <input type="number" value={this.state.C} />
+                        <input type="range" step="1" min="0" max="2000" value={this.state.Contributions} onChange={this.changeCHandler} className="slide" />
+                        <input type="number" value={this.state.Contributions} />
                     </div>
                 </h2>
-                    {/*<h2 className="slideh">What you want to achieve:*/}
-                    {/*    <div className="sliders">*/}
-                    {/*        <input type="range" step="10" min="0" max="10000000" value={this.state.FV} onChange={this.changeFVHandler} className="slide" />*/}
-                    {/*        <input type="number" value={this.state.FV} />*/}
-                    {/*    </div>*/}
-                    {/*</h2>*/}
 
 
                 </div>
 
                 <div>
-                    <Results EBM={this.Monthly().toFixed(2)} EBY={this.Yearly().toFixed(2)} SA={this.state.SA} TC={this.state.C * 12 * this.state.P} TI={this.totalInterest().toFixed(2)}   CIR={null} />
+                    <Results EBM={this.Monthly().toFixed(2)} EBY={this.Yearly().toFixed(2)} SA={this.state.StartingAmount} TC={this.state.Contributions * 12 * this.state.Period} TI={this.totalInterest().toFixed(2)}  />
                 </div>
             </div>
-        // FV={this.futureValue().toFixed(2)}
+
 
         )
     }
